ebea3d208cf4b725dff520b7e53a31cefb96fe15
ArthurCosta15 <arthurcesar252@gmail.com>
2024-03-30 17:16:13 -0300

    adding Scheduling Rules

--- src/repositories/ActivitysRepositories.ts
+++ src/repositories/ActivitysRepositories.ts
@@ -1,4 +1,4 @@
 import { AppDataSource } from "../data-source";
 import { Activity } from "../entities/activitys";
 
-export const ActivitysRepository = AppDataSource.getRepository(Activity)
+export const ActivitysRepositories = AppDataSource.getRepository(Activity)
